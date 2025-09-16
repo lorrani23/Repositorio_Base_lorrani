ba8ca35f3e7f948c536ea695bf9e4efb057c5b38
lorrani23 <lorranigomes.santos@gmail.com>
2025-09-16 11:19:16 -0300

    Delete Modulo 1/Condicionais/for.py

--- Modulo 1/Condicionais/for.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("digite um numero para fazer a tabuada? "))
-numero_inicio = int(input("digite um numero de inicio: "))
-numero_fim = int(input("digite um numero para terminar: "))
-for i in range(1, 11):
-    print(f" {i} x {numero} = {i * numero}")
