5708a32d8dd60b8a57ca5dcabe7deac8daef7cf5
lorrani23 <lorranigomes.santos@gmail.com>
2025-09-16 10:52:10 -0300

    Create float.py

---
+++ Modulo 1/Codigos Basicos/float.py
@@ -0,0 +1,7 @@
+nome_carro = input("Digite o nome do carro: ")
+valor_carro = float(input("Digite o valor do carro: "))
+cor_do_carro = float(input("Digite a cor do carro: "))
+
+print("O nome do carro é" , nome_carro)
+print("O valor do carro é" , valor_carro)
+print("A cor do carro é" , cor_do_carro)
