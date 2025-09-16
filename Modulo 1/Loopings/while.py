c5b4b0d86182d47d8fbb5fefe620b105014f2452
lorrani23 <lorranigomes.santos@gmail.com>
2025-09-16 11:27:51 -0300

    Create while.py

---
+++ Modulo 1/Loopings/while.py
@@ -0,0 +1,14 @@
+nome_cadastro = input("digite seu nome para cadastro: ")
+senha_cadastro = input("digite sua senha para cadastro: ")
+
+nome_digitado = input("digite seu nome: ")
+senha_digitada = input("digite sua senha ")
+
+if nome_cadastro == nome_digitado and senha_digitada == senha_cadastro:
+    print("bem vindo")
+
+else:
+    print("tente nomamente")
+
+while i <= 4322:
+    print("f{i} x {numero} = {i * numero}")
